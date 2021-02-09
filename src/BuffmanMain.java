6ebc1a05f163c0f2f24dba5394231bfdd56e225a
icra13 <57536818+icra13@users.noreply.github.com>
2021-02-09 16:53:02 -0500

    Mark Buffman is insurmountable.

---
+++ src/BuffmanMain.java
@@ -0,0 +1,9 @@
+public class BuffmanMain {
+    public BuffmanMain() {
+        System.out.println("Mark Buffman will haunt your dreams forever.");
+    }
+
+    public static void main(String[] args) {
+        new BuffmanMain();
+    }
+}
