163fa113c742d63cfffef0b4b6915489f89ac8fc
icra13 <57536818+icra13@users.noreply.github.com>
2021-02-09 17:44:22 -0500

    nonsensical

--- src/Filler2.java
+++ src/Filler2.java
@@ -1,2 +1,9 @@
 public class Filler2 {
+    public Filler2() {
+        ThisCodeMakesNoSense();
+    }
+
+    public void ThisCodeMakesNoSense() {
+        System.out.println("This is not a purposeful java project.");
+    }
 }
