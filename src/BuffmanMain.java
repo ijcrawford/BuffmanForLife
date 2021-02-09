4d8b2ffe812fa22f702c88ac0fb9b52ed018430f
icra13 <57536818+icra13@users.noreply.github.com>
2021-02-09 17:40:39 -0500

    changed thing

--- src/BuffmanMain.java
+++ src/BuffmanMain.java
@@ -4,6 +4,7 @@ public class BuffmanMain {
     }
 
     public static void main(String[] args) {
+        System.out.println("Running Buffman enlightenment program");
         new BuffmanMain();
     }
 }
